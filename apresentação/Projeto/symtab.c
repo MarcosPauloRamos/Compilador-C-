ed3ffdf9f6140d78ff4d7ae00ba1a1e46a5f6191
MarcosPauloRamos <49656714+MarcosPauloRamos@users.noreply.github.com>
2021-05-11 16:45:34 -0300

    Update symtab.c

--- apresentação/Projeto/symtab.c
+++ apresentação/Projeto/symtab.c
@@ -12,96 +12,94 @@
 #include <string.h>
 #include "symtab.h"
 
-
-/* SIZE is the size of the hash table */
-#define SIZE 211
-
-/* SHIFT is the power of two used as multiplier
-   in hash function  */
-#define SHIFT 4
-
-/* the hash function */
-static int hash ( char * key )
-{ int temp = 0;
+/* A função hash */
+static int hash ( char * key ){
+  int temp = 0;
   int i = 0;
-  while (key[i] != '\0')
-  { temp = ((temp << SHIFT) + key[i]) % SIZE;
+  while (key[i] != '\0'){ 
+    temp = ((temp << SHIFT) + key[i]) % SIZE;
     ++i;
   }
   return temp;
 }
 
-/* the list of line numbers of the source
- * code in which a variable is referenced
- */
-typedef struct LineListRec
-   { int lineno;
-     struct LineListRec * next;
-   } * LineList;
-
-/* The record in the bucket lists for
- * each variable, including name,
- * assigned memory location, and
- * the list of line numbers in which
- * it appears in the source code
- */
-typedef struct BucketListRec
-   { char * name;
-     dataTypes Dtype;
-     IDTypes IType;
-     char* escopo;
-     LineList lines;
-     int memloc ; /* memory location for variable */
-     struct BucketListRec * next;
-   } * BucketList;
-
-/* the hash table */
-static BucketList hashTable[SIZE];
-
-/* Procedure st_insert inserts line numbers and
- * memory locations into the symbol table
- * loc = memory location is inserted only the
- * first time, otherwise ignored
- */
-void st_insert( char * name, int lineno, int op, char* escopo, dataTypes DType, IDTypes IType )
-{
-  //printf("st_insert\n");
+void st_insert( char * name, int lineno, int op, char* escopo, dataTypes RetType, dataTypes StmtType, IDTypes IType, int vet ){
   int h = hash(name);
   BucketList l =  hashTable[h];
 
+  if(IType == CALL){
+    // Procura a ultima declaração com o mesmo nome
+    while ((l != NULL)){
+      if(((strcmp(name,l->name) == 0))){
+        if(l->IType == CALL){
+          break;
+        }
+      }
+      l = l->next;
+    }
+    if(l == NULL){
+      l = (BucketList) malloc(sizeof(struct BucketListRec));
+      l->name = name;
+      l->lines = (LineList) malloc(sizeof(struct LineListRec));
+      l->lines->lineno = lineno;
+      l->vet = vet;
+      l->memloc = op;
+      l->IType = IType;
+      l->RetType = RetType;
+      l->StmtType = StmtType;
+      l->escopo = escopo;
+      l->lines->next = NULL;
+      l->next = hashTable[h];
+      hashTable[h] = l;
+      return;
+    }
+    else{
+      LineList t = l->lines;
+      while (t->next != NULL) t = t->next;
+      t->next = (LineList) malloc(sizeof(struct LineListRec));
+      t->next->lineno = lineno;
+      t->next->next = NULL;
+      return;
+    }
+  }
 
-  // Procura a ultima declaração com o mesmo nome
+// Procura a ultima declaração com o mesmo nome
   while ((l != NULL) && ((strcmp(name,l->name) != 0))){
     l = l->next;
   }
 
-  //Para inserir: não achou outra declaração, se achou verificar se o escopo é diferente e não é uma função
-  if ( l == NULL || (op != 0 && l->escopo != escopo && l->IType != FUN)) /* variable not yet in table */
-  {
-    l = (BucketList) malloc(sizeof(struct BucketListRec));
-    l->name = name;
-    l->lines = (LineList) malloc(sizeof(struct LineListRec));
-    l->lines->lineno = lineno;
-    l->memloc = op;
-    l->IType = IType;
-    l->Dtype = DType;
-    l->escopo = escopo;
-    l->lines->next = NULL;
-    l->next = hashTable[h];
-    hashTable[h] = l;
+  //Para inserir: não achou outra declaração, se achou verificar se o escopo é DIF e não é uma função
+  if ( l == NULL || (op != 0 && l->escopo != escopo && l->IType != FUN && l->IType != CALL) || IType == RETT) { /* variável não está na tabela ainda */
+    if(l != NULL && strcmp(l->name,name) == 0 && strcmp(l->escopo,"global") == 0){
+      fprintf(listing,N_VERM"[%d] Erro semântico!"RESET" Variável '%s' já declarada no escopo global.\n",lineno,name);
+      Error = TRUE;
+    }else{
+      l = (BucketList) malloc(sizeof(struct BucketListRec));
+      l->name = name;
+      l->lines = (LineList) malloc(sizeof(struct LineListRec));
+      l->lines->lineno = lineno;
+      l->vet = vet;
+      if(IType != RETT) l->memloc = op;
+      else l->memloc = -1;
+      l->IType = IType;
+      l->RetType = RetType;
+      l->StmtType = StmtType;
+      l->escopo = escopo;
+      l->lines->next = NULL;
+      l->next = hashTable[h];
+      hashTable[h] = l;
+      if(IType == PVET || IType == PVAR) insereParam(escopo);
+    }
   }
-
-  else if( l->IType == FUN  && IType == VAR){
-    fprintf(listing,"Erro Semantico: Nome da variavel %s já utilizada como nome de função.[%d]\n",name, lineno);
+  else if( (l->IType == FUN  && IType == VAR) || (l->IType == CALL  && IType == VAR)){
+    fprintf(listing,N_VERM"[%d] Erro semântico!"RESET" Nome da variável '%s' já utilizada como nome de função.\n",lineno,name);
     Error = TRUE;
   }
-  else if( l->escopo == escopo && op != 0)
-  {
-    fprintf(listing,"Erro Semantico: Variavel %s já declarada neste escopo.[%d]\n",name, lineno);
+  else if( l->escopo == escopo && op != 0 ){
+    fprintf(listing,N_VERM"[%d] Erro semântico!"RESET" Variável '%s' já declarada neste escopo.\n",lineno,name);
     Error = TRUE;
-  }
-  else if(l->escopo != escopo && (strcmp(l->escopo,"global") != 0) ){
-    //procura por variavel global entes de supor que não existe
+    
+  }else if(l->escopo != escopo){
     while ((l != NULL)){
       if((strcmp(l->escopo, "global")==0)&& ((strcmp( name,l->name) == 0))){
         LineList t = l->lines;
@@ -114,91 +112,217 @@ void st_insert( char * name, int lineno, int op, char* escopo, dataTypes DType, 
       l = l->next;
     }
     if(l == NULL){
-      fprintf(listing,"Erro Semantico: Variavel %s não declarada neste escopo.[%d]\n",name, lineno);
+      fprintf(listing,N_VERM"[%d] Erro semântico!"RESET" Variável '%s' não declarada neste escopo.\n",lineno,name);
       Error = TRUE;
     }
   }
-  else if(op == 0)
-  {
+  else if(op == 0){
     LineList t = l->lines;
+    if((l->IType != VAR && l->IType != PVAR && l->IType != PVET) && IType == VAR){
+      fprintf(listing,N_VERM"[%d] Erro semântico!"RESET" Vetor '%s' usado como variável.\n",lineno,name);
+      Error = TRUE;
+      return;
+    }else if((l->IType != VET && l->IType != PVET) && IType == VET){
+      fprintf(listing,N_VERM"[%d] Erro semântico!"RESET" Variável '%s' usada como vetor.\n",lineno,name);
+      Error = TRUE;
+      return;
+    }
     while (t->next != NULL) t = t->next;
     t->next = (LineList) malloc(sizeof(struct LineListRec));
     t->next->lineno = lineno;
     t->next->next = NULL;
   }
-} /* st_insert */
+}
 
-/* Function st_lookup returns the memory
- * location of a variable or -1 if not found
- */
-int st_lookup ( char * name )
-{
-  //printf("st_lookup\n");
+int st_lookup ( char * name, char * escopo){
   int h = hash(name);
   BucketList l =  hashTable[h];
-  while ((l != NULL) && (strcmp(name,l->name) != 0))
-    l = l->next;
+  while ((l != NULL) && (strcmp(name,l->name) != 0) && (strcmp(escopo,l->escopo) != 0)){
+    l = l->next;}
   if (l == NULL) return -1;
-  else return l->memloc;
+  else {
+    return 0;
+    }
 }
-void busca_main ()
-{
-  //printf("st_lookup\n");
+
+void busca_main () {
   int h = hash("main");
   BucketList l =  hashTable[h];
   while ((l != NULL) && ((strcmp("main",l->name) != 0 || l->IType == VAR)))
     l = l->next;
   if (l == NULL) {
-    fprintf(listing,"Erro Semantico: Função main não declarada\n");
+    fprintf(listing,N_VERM"     Erro semântico!"RESET" Função main não declarada\n");
     Error = TRUE;
   }
-
 }
+
+dataTypes getFunStmt(char* nome){
+  int h = hash(nome);
+  BucketList l =  hashTable[h];
+  while ((l != NULL)){
+    if (strcmp(nome,l->name) == 0){
+      if (l->IType == FUN) break;
+    }
+    l = l->next;
+  }
+  if (l == NULL) return -1;
+  else return l->StmtType;
+}
+
+int checkReturn(char* escopo){
+  char nome[6] = "return";
+  int h = hash(nome);
+  BucketList l =  hashTable[h];
+  while ((l != NULL)){
+    if (strcmp(nome,l->name) == 0){
+      if (strcmp(escopo,l->escopo) == 0) return 1;
+    }
+    l = l->next;
+  }
+  return -1;
+}
+
 dataTypes getFunType(char* nome){
   int h = hash(nome);
   BucketList l =  hashTable[h];
-  while ((l != NULL) && (strcmp(nome,l->name) != 0))
-    l = l->next;
 
+  while ((l != NULL)){
+    if (strcmp(nome,l->name) == 0){
+      if (l->IType == FUN) break;
+    }
+    l = l->next;
+  }
   if (l == NULL) return -1;
-  else return l->Dtype;
+  else return l->RetType;
 }
 
-/* Procedure printSymTab prints a formatted
- * listing of the symbol table contents
- * to the listing file
- */
-void printSymTab(FILE * listing)
-{
-  //printf("printSymtab\n");
+int getMemLoc(char* nome, char* escopo){
+  int h = hash(nome);
+  BucketList l =  hashTable[h];
+  while ((l != NULL)){
+    if (strcmp(nome,l->name) == 0){
+      if(strcmp(escopo,l->escopo) == 0) break;
+    }
+    l = l->next;
+  }
+  if (l == NULL) return -1;
+  else return l->memloc;
+}
+
+IDTypes getVarType(char* nome, char* escopo){
+  int h = hash(nome);
+  BucketList l =  hashTable[h];
+  if(nome == NULL) return -1;
+  while ((l != NULL)){
+    if (strcmp(nome,l->name) == 0){
+      if(strcmp(escopo,l->escopo) == 0) break;
+    }
+    l = l->next;
+  }
+  if (l == NULL) return -1;
+  else return l->IType;
+}
+
+int getNumParam(char *nome){
+  int h = hash(nome);
+  BucketList l =  hashTable[h];
+  if(nome == NULL) return -1;
+  while ((l != NULL)){
+    if (strcmp(nome,l->name) == 0){
+      if(l->IType == FUN) break;
+    }
+    l = l->next;
+  }
+  if (l == NULL) return -1;
+  else return l->tam;
+}
+
+void insereParam(char *nome){
+  int h = hash(nome);
+  BucketList l =  hashTable[h];
+  if(nome == NULL) return;
+  while ((l != NULL)){
+    if (strcmp(nome,l->name) == 0){
+      if(l->IType == FUN) break;
+    }
+    l = l->next;
+  }
+  if (l == NULL) return;
+  else l->tam++;
+}
+
+void printSymTab(FILE * listing) {
   int i;
-  fprintf(listing,"Variable Name  Escopo  Tipo ID  Tipo dado  Line Numbers\n");
-  fprintf(listing,"-------------  ------  -------  ---------  ------------\n");
-  for (i=0;i<SIZE;++i)
-  { if (hashTable[i] != NULL)
-    { BucketList l = hashTable[i];
-      while (l != NULL)
-      { LineList t = l->lines;
+  fprintf(listing,"---------------------------------------------------------------------------------\n");
+  fprintf(listing,"Nome           Escopo  Tipo ID  Tipo Retorno  Tipo Param  Mem. Loc.  Num da linha\n");
+  fprintf(listing,"-------------  ------  -------  ------------  ----------  ---------  ------------\n");
+  for (i=0;i<SIZE;++i) {
+    if (hashTable[i] != NULL) {
+      BucketList l = hashTable[i];
+      while (l != NULL) {
+        LineList t = l->lines;
         fprintf(listing,"%-14s ",l->name);
         fprintf(listing,"%-6s  ",l->escopo);
         char* id, *data;
-        if(l->IType == VAR){
-          id = "VAR";
+        switch(l->IType){
+          case RETT:
+            id = "ret";
+            break;
+          case VAR:
+            id = "var";
+            break;
+          case PVAR:
+            id = "pvar";
+            break;
+          case FUN:
+             id = "fun";
+            break;
+          case CALL:
+             id = "call";
+            break;
+          case VET:
+            id= "vet";
+            break;
+          case PVET:
+            id= "pvet";
+            break;
+          default:
+          break;
         }
-        else if(l->IType == FUN){
-          id = "FUN";
-        }
-        if(l->Dtype == INTTYPE){
-          data = "INT";
-        }
-        else if(l->Dtype == VOIDTYPE){
-          data = "VOID";
+        switch(l->RetType){
+          case INTTYPE:
+            data= "INT";
+          break;
+          case VOIDTYPE:
+            data= "VOID";
+          break;
+          case NULLL:
+            data = "null";
+          break;
+          default:
+          break;
         }
         fprintf(listing,"%-7s  ",id);
-        fprintf(listing,"%-8s  ",data);
+        fprintf(listing,"%-12s  ",data);
 
-        while (t != NULL)
-        { fprintf(listing,"%3d; ",t->lineno);
+        switch(l->StmtType){
+          case INTTYPE:
+            data= "INT";
+          break;
+          case VOIDTYPE:
+            data= "VOID";
+          break;
+          case NULLL:
+            data = "null";
+          break;
+          default:
+          break;
+        }
+        fprintf(listing,"%-10s ",data);
+        if(l->memloc >= 0) fprintf(listing,"    %-3d     ",l->memloc);
+        else fprintf(listing,"    -       ");
+        while (t != NULL) {
+          fprintf(listing,"%3d; ",t->lineno);
           t = t->next;
         }
         fprintf(listing,"\n");
