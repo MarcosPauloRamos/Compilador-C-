28d66d1d1db45c7e1015d49f2e8734d440d57712
MarcosPauloRamos <49656714+MarcosPauloRamos@users.noreply.github.com>
2021-07-29 09:55:21 -0300

    Update binary.c

--- apresentação/Projeto/binary.c
+++ apresentação/Projeto/binary.c
@@ -17,12 +17,49 @@ const char *opcodeBins[] =   {"000000", "000000", "000000", "000000", "000000", 
 
 const char *functBins[] = { "000000", "000001", "000010", "000011", "000100", "000101", "000110", "000111", "001000", "001001", "001010" };
 
+/*int transformacaobinario (int bin, int tamanho, char *nbinario){
+    int x[32];
+    char aux[50], n[50];
+    int i, j, numero = bin;
+    for (i = 0; i < 32; i++) x[i] = 0;
+    i = 31;
+    while (numero > 1){
+        x[i] = numero%2;
+        numero = numero/2;
+        i--;
+    }
+    x[i] = numero%2;
+    n[0] ='\0';
+    for (j = 32 - tamanho; j < 32; j++){
+        sprintf(aux, "%d", x[j]);
+        strcat(n, aux);
+    }
+    strcpy (nbinario, n);
+    return 0;
+}*/
+
 void assembly_binary(AssemblyCode codeLine){
     Instruction inst;
+    char r1[5], r2[5], r3[5],ri[26];
     if(codeLine->kind == instr){
         inst = codeLine->line.instruction;
         switch(inst.format){
         case formatR:
+            /*
+            transformacaobinario(inst.reg1, 5, r1);
+            transformacaobinario(inst.reg2, 5, r2);
+            transformacaobinario(inst.reg3, 5, r3);
+            transformacaobinario(0, 5, ri);
+            fprintf(listing,"ram[%d] = {6'b%s, 5'b%s, 5'b%s, 5'b%s, 5'b%s, 6'b%s};",codeLine->lineno,
+                                                             opcodeBins[inst.opcode],
+                                                             r2,
+                                                             rg3,
+                                                             r1,
+                                                             ri,
+                                                             functBins[inst.opcode]);
+            fprintf(listing,"   // %s\n",Prefixos[inst.opcode]);
+            break;
+            */
             fprintf(listing,"ram[%d] = {6'b%s, 5'd%d, 5'd%d, 5'd%d, 5'd0, 6'b%s};",codeLine->lineno,
                                                              opcodeBins[inst.opcode],
                                                              inst.reg2,
@@ -33,10 +70,22 @@ void assembly_binary(AssemblyCode codeLine){
             break;
         case formatJ:
             if(inst.opcode == jst){
+              /*
+              transformacaobinario(0, 26, ri);
+                fprintf(listing,"ram[%d] = {6'b%s, 26'b%s};",codeLine->lineno,
+                                                           opcodeBins[inst.opcode],
+                                                           ri);
+              */
                 fprintf(listing,"ram[%d] = {6'b%s, 26'd0};",codeLine->lineno,
                                                            opcodeBins[inst.opcode]);
                 fprintf(listing,"   // %s\n",Prefixos[inst.opcode]);
             }else{
+              /*
+              transformacaobinario(inst.imed, 26, ri);
+                fprintf(listing,"ram[%d] = {6'b%s, 26'b%s};",codeLine->lineno,
+                                                           opcodeBins[inst.opcode],
+                                                           ri);
+              */
                 fprintf(listing,"ram[%d] = {6'b%s, 26'd%d};",codeLine->lineno,
                                                            opcodeBins[inst.opcode],
                                                            inst.imed);
@@ -45,12 +94,32 @@ void assembly_binary(AssemblyCode codeLine){
             break;
         case formatI:
             if(inst.opcode == sti || inst.opcode == ldi){
+              /*
+            transformacaobinario(inst.reg1, 5, r1);
+            transformacaobinario(0, 5, r2);
+            transformacaobinario(inst.imed, 16, ri);
+                fprintf(listing,"ram[%d] = {6'b%s, 5'b%s, 5'b%s, 16'b%s};",codeLine->lineno,
+                                                           opcodeBins[inst.opcode],
+                                                           r2,
+                                                           r1,
+                                                           ri);
+              */
                 fprintf(listing,"ram[%d] = {6'b%s, 5'd0, 5'd%d, 16'd%d};",codeLine->lineno,
                                                            opcodeBins[inst.opcode],
                                                            inst.reg1,
                                                            inst.imed);
                 fprintf(listing,"   // %s\n",Prefixos[inst.opcode]);
             }else if(inst.opcode == bgt || inst.opcode == blt){
+              /*
+            transformacaobinario(inst.reg1, 5, r1);
+            transformacaobinario(inst.reg2, 5, r2);
+            transformacaobinario(inst.imed, 16, ri);
+                fprintf(listing,"ram[%d] = {6'b%s, 5'b%s, 5'b%s, 16'b%s};",codeLine->lineno,
+                                                           opcodeBins[inst.opcode],
+                                                           r1,
+                                                           r2,
+                                                           ri);
+              */
                 fprintf(listing,"ram[%d] = {6'b%s, 5'd%d, 5'd%d, 16'd%d};",codeLine->lineno,
                                                            opcodeBins[inst.opcode],
                                                            inst.reg1,
@@ -58,6 +127,16 @@ void assembly_binary(AssemblyCode codeLine){
                                                            inst.imed);
                 fprintf(listing,"   // %s\n",Prefixos[inst.opcode]);
             }else if(inst.opcode == mov || inst.opcode == put){
+              /*
+            transformacaobinario(inst.reg1, 5, r1);
+            transformacaobinario(inst.reg2, 5, r2);
+            transformacaobinario(inst.imed, 16, ri);
+                fprintf(listing,"ram[%d] = {6'b%s, 5'b%s, 5'b%s, 16'b%s};",codeLine->lineno,
+                                                           opcodeBins[inst.opcode],
+                                                           r2,
+                                                           r1,
+                                                           ri);
+              */
                 fprintf(listing,"ram[%d] = {6'b%s, 5'd%d, 5'd%d, 16'd%d};",codeLine->lineno,
                                                            opcodeBins[addi],
                                                            inst.reg2,
@@ -65,6 +144,16 @@ void assembly_binary(AssemblyCode codeLine){
                                                            inst.imed);
                 fprintf(listing,"   // %s\n",Prefixos[inst.opcode]);
             }else{
+              /*
+            transformacaobinario(inst.reg1, 5, r1);
+            transformacaobinario(inst.reg2, 5, r2);
+            transformacaobinario(inst.imed, 16, ri);
+                fprintf(listing,"ram[%d] = {6'b%s, 5'b%s, 5'b%s, 16'b%s};",codeLine->lineno,
+                                                           opcodeBins[inst.opcode],
+                                                           r2,
+                                                           r1,
+                                                           ri);
+              */
                 fprintf(listing,"ram[%d] = {6'b%s, 5'd%d, 5'd%d, 16'd%d};",codeLine->lineno,
                                                            opcodeBins[inst.opcode],
                                                            inst.reg2,
@@ -75,14 +164,37 @@ void assembly_binary(AssemblyCode codeLine){
             break;
         case formatSYS:
             if(inst.opcode == halt || inst.opcode == sleep || inst.opcode == wake){
+              /*
+              transformacaobinario(0, 26, ri);
+              fprintf(listing,"ram[%d] = {6'b%s, 26'b%s};",codeLine->lineno,
+                                                           opcodeBins[inst.opcode],
+                                                           ri);
+              */
                 fprintf(listing,"ram[%d] = {6'b%s, 26'd0};",codeLine->lineno,
                                                            opcodeBins[inst.opcode]);
                 fprintf(listing,"   // %s\n",Prefixos[inst.opcode]);
             }else if(inst.opcode == ctso){
+              /*
+              transformacaobinario(END_SWITCH, 26, ri);
+              fprintf(listing,"ram[%d] = {6'b%s, 26'b%s};",codeLine->lineno,
+                                                           opcodeBins[inst.opcode],
+                                                           ri);
+              */
                 fprintf(listing,"ram[%d] = {6'b%s, 26'd%d};",codeLine->lineno,
                                                            opcodeBins[jal],END_SWITCH);
                 fprintf(listing,"   // %s\n",Prefixos[inst.opcode]);
             }else{
+              /*
+              transformacaobinario(0, 5, r1);
+            transformacaobinario(inst.reg2, 5, r2);
+            transformacaobinario(inst.reg3, 5, r3);
+            transformacaobinario(0, 16, ri);
+              fprintf(listing,"ram[%d] = {6'b%s, 5'b%s, 5'b%s, 16'b%s};",codeLine->lineno,
+                                                           opcodeBins[inst.opcode],
+                                                           r1,
+                                                           r2,
+                                                           ri);
+              */
                 fprintf(listing,"ram[%d] = {6'b%s, 5'd0, 5'd%d, 16'd0};",codeLine->lineno,
                                                            opcodeBins[inst.opcode],
                                                            inst.reg2);
